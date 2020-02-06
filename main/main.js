7865f854999ce2af291159e54fc7932130d3f7df
azamlynny <azamlynny@hotmail.com>
2020-02-06 12:58:10 -0500

    Removed multiple tabs due to issues

--- main/main.js
+++ main/main.js
@@ -1,6 +1,27 @@
+class Button {
+  
+  constructor(x, y, wd, ht, state) {
+    this.x = x;
+    this.y = y;
+    this.wd = wd;  
+    this.ht = ht;
+    this.state = state;
+  }
+  
+  //// Getter
+  //get area() {
+  //  return this.calcArea();
+  //}
+
+  // Method
+  drawButton() {
+    rect(this.x,this.y,this.wd,this.ht);
+  }
+}
+
 var wd;
 var ht;
-const b1 = new button(10, 10, 50, 200, 0);
+const b1 = new Button(10, 10, 50, 200, 0);
 
 function setup() {
   frameRate(60);
